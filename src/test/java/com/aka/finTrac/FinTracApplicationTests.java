f4ae36f0e8dea0c8b6488e34e170bcc62967cbac
Muhammad Arslan - IT <906427@pk.ubl.int>
2023-09-12 19:04:05 +0500

    initial commit

---
+++ src/test/java/com/aka/finTrac/FinTracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aka.finTrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
